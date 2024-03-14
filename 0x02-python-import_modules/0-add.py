0644b8b36f8254133015ea1953548c034a08cd4e
Meargteame <mearegteame99995555@gmail.com>
2024-03-15 02:19:17 +0300

    Tak 0

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,7 +1,7 @@
-#!/usr/bin/python3
-from add_0 import add
+#!/user/bin/python3
+if __name__ =="__main__":
+    from add_0 import add
 a = 1
 b = 2
-result = add(a, b)
-print(f"{a} + {b} = {result}",end=" ")
+print("{} + {} = {}".format(a,b, add(a,b)))
 
