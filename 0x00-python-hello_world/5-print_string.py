747f88a0c8de1e559a39f79289b08141ad9078e9
meargteame <mearegteame99995555@gmail.com>
2024-05-21 15:54:52 -0400

    commited

--- 0x00-python-hello_world/5-print_string.py
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/python3
-str = "ALX School"
-print(3 * str)
-print(str[:9])
--- 0x03-python-data_structures/0-print_list_integer.py
+++ 0x03-python-data_structures/0-print_list_integer.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-print_list_integer = __import__('0-print_list_integer').print_list_integer
+
 
 my_list = [1, 2, 3, 4, 5]
 print_list_integer(my_list)
@@ -9,4 +9,3 @@ def print_list_integer(my_list=[]):
         print(item)
 my_list=[1,2,4,5]
 print_list_integer(my_list)
-
