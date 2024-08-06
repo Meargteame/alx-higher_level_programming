12fe09f20dfab673a36afa18e4bfaa7ca395cf50
Meargteame <mearegteame99995555@gmail.com>
2024-08-06 21:39:43 +0300

    tasks updated

--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -1,17 +1,15 @@
-import sqlalchemy 
 from sqlalchemy import create_engine
 from sqlalchemy import Column , Integer , String 
 from sqlalchemy.orm import declarative_base
-from sqlalchemy.orm import sessionmaker
 
 Base = declarative_base()
 
 class State(Base):
     __tablename__ = 'states'
-    id = Column(Integer,autoincrement=True, primary_key=True,unique=True, nullable=False)
+    id = Column(Integer,autoincrement=True, primary_key=True,nullable=False)
     name =Column(String(128),nullable=False)
 
 
 if __name__ == '__main__':
-    engine = create_engine("mysql://root:thisis1yearplan2025@localhost:3306/hbtn_0e_6_usa")
+    engine = create_engine("mysql+mysqldb://root:thisis1yearplan2025@localhost:3306/hbtn_0e_6_usa")
     Base.metadata.create_all(engine)
