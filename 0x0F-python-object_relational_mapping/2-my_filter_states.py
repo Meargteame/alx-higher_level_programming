3cef86afd2653d7372c8b373962b44407c84ca96
Meargteame <mearegteame99995555@gmail.com>
2024-08-06 20:15:13 +0300

    tasks updated

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -1,21 +1,22 @@
 #!/usr/bin/python3
 """
 This module connects to a MySQL database and lists all states
-from the `hbtn_0e_0_usa` database.
+from the `hbtn_0e_0_usa` database where the state name matches
+the given argument.
 """
 import MySQLdb
 import sys
 
 
-state_name=sys.argv[4]
-
-def list_states(user, password, database):
+def list_states(user, password, database, state_name):
     """
-    Connect to the MySQL database and list all states.
+    Connect to the MySQL database and list all states with a name matching state_name.
+    
     Args:
         user (str): The MySQL username.
         password (str): The MySQL password.
         database (str): The name of the database to connect to.
+        state_name (str): The name of the state to filter by.
     """
     db = MySQLdb.connect(
         host="localhost",
@@ -25,10 +26,9 @@ def list_states(user, password, database):
         port=3306
     )
     cur = db.cursor()
-    cur.execute(
-             "SELECT id, name FROM states WHERE name = '{}' ORDER BY id ASC".format(state_name)
-
-        )
+    # Use format method to insert the state name into the query
+    query = "SELECT id, name FROM states WHERE name = '{}' ORDER BY id ASC".format(state_name)
+    cur.execute(query)
     rows = cur.fetchall()
 
     for row in rows:
@@ -40,6 +40,6 @@ def list_states(user, password, database):
 
 if __name__ == '__main__':
     if len(sys.argv) != 5:
-        print("Usage: ./select_states.py <user> <password> <database>")
+        print("Usage: ./2-my_filter_states.py <user> <password> <database> <state_name>")
     else:
-        list_states(sys.argv[1], sys.argv[2], sys.argv[3])
+        list_states(sys.argv[1], sys.argv[2], sys.argv[3], sys.argv[4])
