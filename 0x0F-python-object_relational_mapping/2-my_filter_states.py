18391e4fbcd02f66b32470bd6fe7f41166215283
Meargteame <mearegteame99995555@gmail.com>
2024-08-06 20:22:12 +0300

    tasks updates

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -10,8 +10,9 @@ import sys
 
 def list_states(user, password, database, state_name):
     """
-    Connect to the MySQL database and list all states with a name matching state_name.
-    
+    Connect to the MySQL database and list all states with a name
+    matching state_name.
+
     Args:
         user (str): The MySQL username.
         password (str): The MySQL password.
@@ -27,7 +28,10 @@ def list_states(user, password, database, state_name):
     )
     cur = db.cursor()
     # Use format method to insert the state name into the query
-    query = "SELECT id, name FROM states WHERE name = '{}' ORDER BY id ASC".format(state_name)
+    query = (
+        "SELECT id, name FROM states WHERE name = '{}' "
+        "ORDER BY id ASC"
+    ).format(state_name)
     cur.execute(query)
     rows = cur.fetchall()
 
@@ -38,8 +42,13 @@ def list_states(user, password, database, state_name):
     db.close()
 
 
+usage = (
+    "Usage: ./2-my_filter_states.py <user> <password> <database> "
+    "<state_name>"
+)
+
 if __name__ == '__main__':
     if len(sys.argv) != 5:
-        print("Usage: ./2-my_filter_states.py <user> <password> <database> <state_name>")
+        print(usage)
     else:
         list_states(sys.argv[1], sys.argv[2], sys.argv[3], sys.argv[4])
